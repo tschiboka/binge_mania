d34987f57105f3b39635eec0380007f8621ec2c0
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-09-22 01:21:40 +0100

    start sass reset css

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">HERE</header>
+      </div>
+    );
+  }
+};
