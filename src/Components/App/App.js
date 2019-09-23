1b9eb2c2733dfed3b05bfd01ce9415ac3b17edf1
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-09-23 13:45:27 +0100

    basic header

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,7 +5,17 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">HERE</header>
+        <header className="App-header">
+          <div className="App-header__logo">Binge Mania</div>
+
+          <div className="App-header__icons">
+            <div className="App-header__icons__genres">Genres</div>
+
+            <div className="App-header__icons__shopping-cart">Cart</div>
+
+            <div className="App-header__icons__user">User</div>
+          </div>
+        </header>
       </div>
     );
   }
