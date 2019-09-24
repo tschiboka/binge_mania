a4433ab963a1333e86b7f67846511e71fe200424
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-09-24 23:13:48 +0100

    custom dropdown menu with react

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,6 +10,8 @@ export default class Header extends Component {
 
 
     componentDidMount() {
+        window.addEventListener('resize', this.setGenresOptionsCoords);
+
         const getGenres = async () => {
             try {
                 const response = await fetch("/api/genres");
@@ -24,11 +26,33 @@ export default class Header extends Component {
 
 
 
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.setGenresOptionsCoords);
+    }
+
+
+
     handleGenresMenuClick() {
         const newState = this.state;
         newState.openGenreOptions = !newState.openGenreOptions;
         this.setState(newState);
+
+        this.setGenresOptionsCoords();
         console.log("Click", this.state.openGenreOptions);
+    }
+
+
+
+    setGenresOptionsCoords() {
+        // adjust options div coordinates to genres
+        const genresDiv = document.getElementById("Header__genres");
+        const genreRect = genresDiv.getBoundingClientRect();
+        const headerDiv = document.getElementsByClassName("Header")[0];
+        const headerRect = headerDiv.getBoundingClientRect();
+        const genresOpts = document.getElementById("Header__genres__options");
+
+        genresOpts.style.left = genreRect.left + "px";
+        genresOpts.style.top = headerRect.bottom + "px";
     }
 
 
@@ -44,8 +68,11 @@ export default class Header extends Component {
                         onClick={() => this.handleGenresMenuClick()}
                     >
                         Genres {this.state.openGenreOptions
-                            ? <span>&#9662;</span> : <span>&#9652;</span>}
-                        <div id="Header__genres__options" >
+                            ? <span>&#9652;</span> : <span>&#9662;</span>}
+
+                        <div
+                            id="Header__genres__options"
+                            style={{ visibility: this.state.openGenreOptions ? "visible" : "hidden" }}>
                             <div className="Header__genres__option">Comedy</div>
 
                             <div className="Header__genres__option">Horror</div>
