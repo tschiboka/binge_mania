811f916e598c06882accc837887e66085150daa1
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-10-24 18:45:20 +0100

    MovieDetails components title and close btn

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -11,7 +11,22 @@ export default class MovieDetails extends Component {
                     onBlur={() => this.props.showMovieDetails(false, this.props.movie)}
                 >
                     <div className="MovieDetails__main--inner">
-                        <button onClick={() => this.props.showMovieDetails(false, this.props.movie)}>X</button>
+                        <div className="MovieDetails__title--outer" >
+                            <div className="MovieDetails__title--inner">
+                                {this.props.movie.title}
+                            </div>
+                        </div>
+
+                        <div className="MovieDetails__btn-box--outer">
+                            <div className="MovieDetails__btn-box--middle">
+                                <div className="MovieDetails__btn-box--inner">
+                                    <div className="MovieDetails__btn-box__btn-base">
+                                        <button onClick={() => this.props.showMovieDetails(false, this.props.movie)}>&times;</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
