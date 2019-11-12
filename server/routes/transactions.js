6e8dc7351bf9a734782f7e1e00b868d36d279c60
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-11-12 14:37:22 +0000

    post transaction is complete, update user and create new transaction at the same time

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -2,6 +2,12 @@ const express = require("express");
 const route = express.Router();
 const { Transaction, validate } = require("../models/transaction");
 const { User } = require("../models/user");
+const mongoose = require("mongoose");
+const Fawn = require("fawn");
+
+
+
+Fawn.init(mongoose);
 
 
 
@@ -17,9 +23,23 @@ route.post("/", async (req, res) => {
         if (error) return res.status(400).send(error.details[0].message);
 
         const user = await User.findById(req.body.user._id);
+        if (!user) return res.status(400).send("No user with the given id " + req.body.user._id);
 
+        const transaction = new Transaction({
+            movies: req.body.movies,
+            transTotal: req.body.transTotal,
+            user: { id: user._id, email: user.email }
+        });
+        console.log("ID HERE");
+        console.log(transaction._id);
 
-        res.send(user);
+        // FAWN transaction here (an easier implementation of 2 phase commit)
+        new Fawn.Task()
+            .save("transactions", transaction) // we need to use the actual name of the collection
+            .update("users", { _id: user._id }, { $push: { transactions: transaction._id } })
+            .run();
+
+        res.send(transaction);
     } catch (err) { res.status(500).send("ERROR: " + err) }
 });
 
