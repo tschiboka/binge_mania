c0e540fd3da0a360e9ca054802d8a0a9b27a95a9
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-11-14 12:16:41 +0000

    History now has bottom, loadingSpinner, content container

--- src/Components/History/History.js
+++ src/Components/History/History.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 import "./History.scss";
 
 
 export default class History extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { isLoading: false }
+    }
+
+
+
     render() {
         return (
             <div className="History">
@@ -26,6 +35,14 @@ export default class History extends Component {
                                     </div>
                                 </div>
                             </div>
+                        </div>
+
+                        <div className="History__content"></div>
+
+                        <LoadingSpinner isLoading={this.state.isLoading} />
+
+                        <div className="History__frame--bottom--outer">
+                            <div className="History__frame--bottom--middle"></div>
                         </div>
                     </div>
                 </div>
