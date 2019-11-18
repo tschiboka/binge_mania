b0133db7192e4538d41186a39fa226cd07310f81
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-11-18 20:20:55 +0000

    basic haeder on AdminTransactions

--- src/Components/AdminTransactions/AdminTransactions.js
+++ src/Components/AdminTransactions/AdminTransactions.js
@@ -4,9 +4,23 @@ import "./AdminTransactions.scss";
 
 
 export default class AdminTransactions extends Component {
+
     render() {
         return (
-            <div className="AdminTransactions">Transactions</div>
+            <div className="AdminTransactions">
+                <div className="AdminTransactions__header">
+                    <form>
+                        <div className="AdminTransactions__filter-by">
+                            <input type="text" />
+
+                            <button>&#9660;</button>
+
+                            <button>Filter</button>
+                        </div>
+                        <button>Search</button>
+                    </form>
+                </div>
+            </div>
         );
     }
 }
