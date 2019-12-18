3b985faf7f22cbc830790563abc16dd542c72746
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-12-18 23:12:19 +0000

    basic layout of AdminGenres

--- src/Components/AdminGenres/AdminGenres.js
+++ src/Components/AdminGenres/AdminGenres.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 import "./AdminGenres.scss";
+import { spawn } from 'child_process';
 
 export default class AdminGenres extends Component {
     constructor(props) {
@@ -21,8 +22,32 @@ export default class AdminGenres extends Component {
             const response = await fetch("/api/genres");
             const genresJSON = await response.json();
 
-            this.setState({ ...this.state, genres: genresJSON, isLoading: false });
+            this.setState({
+                ...this.state,
+                genres: genresJSON.sort((a, b) => b.moviesWithGenre - a.moviesWithGenre),
+                isLoading: false
+            });
         } catch (err) { console.log(err); }
+    }
+
+
+
+    renderGenresTable() {
+        return (this.state.genres || []).map((genre, i) => (
+            <div className="AdminGenres__genre" key={"AdminGenre" + i}>
+                <div>{genre.name}</div>
+
+                <div>{genre.moviesWithGenre}</div>
+
+                <div>{genre.showInMenu + ""}</div>
+
+                <div>
+                    <div className={"AdminGenres-genre--" + (genre.showInMenu ? "checked" : "unchecked")}>
+                        {genre.showInMenu ? <span>&#10004;</span> : <span>&times;</span>}
+                    </div>
+                </div>
+            </div>
+        ));
     }
 
 
@@ -30,7 +55,11 @@ export default class AdminGenres extends Component {
     render() {
         return (
             <div className="AdminGenres">
-                GENREEES
+                <div className="AdminGenres__box">
+                    <div className="AdminGenres__header"><div>genre</div><div>movies</div><div>visible</div></div>
+                    {this.renderGenresTable()}
+                </div>
+
                 <LoadingSpinner isLoading={this.state.isLoading} />
             </div>
         );
